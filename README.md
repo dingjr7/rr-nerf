b88c2313c9f2f3857020b31e8ceb89d94a36cd42
dingjr <dingjr@shanghaitech.edu.cn>
2024-06-27 17:35:36 +0800

    Add md

--- README.md
+++ README.md
@@ -1,9 +1,30 @@
-# JaxNeRF
+# Ray Reordering for Hardware-Accelerated Neural Volume Rendering
+This repository hosts the training code for the research detailed in the following paper:
 
-## Proposal
+```
 
-The project is based on jax and hiaku,
-aiming to provide python interface for training(quantization), rendering and hardware verfication.
+```
 
-Besides implementing origin instant ngp algorithm,
-i will implement new octree based algorithm with jax for further quantization and verfication.
+## Requirements
+```
+sudo apt-get install build-essential git python3-dev python3-pip libopenexr-dev libxi-dev \
+                     libglfw3-dev libglew-dev libomp-dev libxinerama-dev libxcursor-dev pybind11-dev libeigen3-dev
+```
+## Compilation
+```
+git clone https://github.com/dingjr7/rr-nerf.git
+cd rr-nerf
+git submodule sync --recursive
+git submodule update --init --recursive
+```
+
+```
+conda env create --file environment.yml
+conda activate rr
+```
+
+## Demo
+Modify dataset_dir to nerf_synthetic dataset.
+```
+python train_envr.py
+```
--- train_envr.py
+++ train_envr.py
@@ -8,16 +8,17 @@ from jax import dlpack, numpy as jnp
 import os
 import shutil
 
-os.environ['CUDA_VISIBLE_DEVICES'] = '0'
-os.environ['XLA_PYTHON_CLIENT_PREALLOCATE'] = "true"
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+os.environ["XLA_PYTHON_CLIENT_PREALLOCATE"] = "true"
 
 # dataset_dir = "../baseline/data/nerf_synthetic"
-dataset_dir = "../nerf_synthetic"
+dataset_dir = "/dataset/nerf_synthetic"
 
 # 输出目录设置
 output_dir = "output"
 if not os.path.exists(output_dir):
     os.mkdir(output_dir)
+
 
 def train(dataset_name):
     config = toml.load("configs/ngp/base.toml")
@@ -28,8 +29,10 @@ def train(dataset_name):
         {
             "config": config,
             "density_grid": density_grid,
-            "ngp_params": snapshot["params"]
-        }, dataset)
+            "ngp_params": snapshot["params"],
+        },
+        dataset,
+    )
     optimizer = optax.adam(learning_rate=1e-2, b1=0.9, b2=0.99, eps=1e-15)
     opt_state = optimizer.init(trainer.trainable_params)
 
@@ -43,16 +46,23 @@ def train(dataset_name):
         for j in range(0, 35000):
             loss, grads = trainer.train_step(1 << 18)
             opt_state, trainer.trainable_params = update_with(
-                grads, opt_state, trainer.trainable_params)
+                grads, opt_state, trainer.trainable_params
+            )
             pbar.update(1)
-            pbar.set_description(f'Loss: {-10 * jnp.log10(loss)} dB')
+            pbar.set_description(f"Loss: {-10 * jnp.log10(loss)} dB")
 
     trainer.save_snapshot(f"{output_dir}/{dataset_name}.bin")
 
 
 if __name__ == "__main__":
     for name in [
-            "chair", "lego", "drums", "ficus", "hotdog", "materials", "mic",
-            "ship"
+        "chair",
+        "lego",
+        "drums",
+        "ficus",
+        "hotdog",
+        "materials",
+        "mic",
+        "ship",
     ]:
         train(name)
